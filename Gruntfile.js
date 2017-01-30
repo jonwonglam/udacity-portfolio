11595f21158508079b6b2a143a555dddf2db3ab0
Jon Wong-Lam <jonwonglam@gmail.com>
2017-01-30 13:30:09 -0800

    add logo and animations

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,58 +9,24 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: 'im',
-          sizes: [{
-            width: 1000,
-            suffix: '_large_3x',
-            quality: 20
-          },
-          {
-            width: 400,
-            suffix: '_large_1x',
-            quality: 30
-          }
-        ]
+    autoprefixer: {
+            dist: {
+                files: {
+                    'build/main.css': 'css/main.css'
+                }
+            }
         },
 
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images_src/',
-          dest: 'images/'
-        }]
-      }
-    },
+    watch: {
+                styles: {
+                    files: ['css/main.css'],
+                    tasks: ['autoprefixer']
+                }
+            },
 
     /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['images']
-        },
-      },
-    },
-
   });
 
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
-
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 };
